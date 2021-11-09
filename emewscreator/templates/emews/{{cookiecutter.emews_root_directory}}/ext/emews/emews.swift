ae0ce515accb7d3c48afe060679363c52ce4d6be
Nick Collier <ncollier@anl.gov>
2021-11-09 13:47:45 -0500

    Added error check

--- emewscreator/templates/emews/{{cookiecutter.emews_root_directory}}/ext/emews/emews.swift
+++ emewscreator/templates/emews/{{cookiecutter.emews_root_directory}}/ext/emews/emews.swift
@@ -13,6 +13,12 @@ params_str = ';'.join(str_vals)
 """;
 
 (string vals[]) parse_json_list(string json_list) {
+    // Python stack trace can be passed in as the parameter list
+    // if ME itself raises an Error. This checks for that
+    // and prints that error.
+    if (find(json_list, "Traceback (", 0, -1) == 0) {
+        printf(json_list);
+    }
     string code = split_json_template % json_list;
     string params_str = python_persist(code, "params_str");
     vals = split(params_str, ";");
